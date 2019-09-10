933890e31df97cadb5a4bb92410a28c3841995d2
Vlad Ryabinin <vladislav.rjabinin@ylab.io>
2019-09-10 14:38:18 +0600

    Created webcam broadcast with snapshot func.

--- README.md
+++ README.md
@@ -1,21 +1,4 @@
 
-## Available Scripts
+# Webcam broadcast with snapshot func
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
+Made with ❤️ just for testing WebRTC API.
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,11 +1,137 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NotFound = () => {
-  return (
-    <div>
-      <div>Home</div>
-    </div>
-  );
-};
+import styles from './style.scss';
 
-export default NotFound;
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCanvasFilled: false,
+    };
+
+    this.video = React.createRef();
+    this.canvas = React.createRef();
+    this.startButton = React.createRef();
+
+    this.width = 320;
+    this.height = 0;
+
+    this.streaming = false;
+  }
+
+  componentDidMount() {
+    navigator.getMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia;
+
+    navigator.getMedia(
+      { video: true, audio: false },
+      stream => {
+        this.video.current.srcObject = stream;
+        this.video.current.play();
+      },
+      error => console.warn('An error occurred:', error),
+    );
+  }
+
+  handleCanPlay = () => {
+    if (!this.streaming) {
+      this.height = this.video.current.videoHeight / (this.video.current.videoWidth / this.width);
+
+      this.video.current.setAttribute('width', this.width);
+      this.video.current.setAttribute('height', this.height);
+      this.canvas.current.setAttribute('width', this.width);
+      this.canvas.current.setAttribute('height', this.height);
+      this.streaming = true;
+    }
+  };
+
+  handleClick = e => {
+    e.preventDefault();
+    const context = this.canvas.current.getContext('2d');
+
+    if (this.width && this.height) {
+      this.canvas.current.width = this.width;
+      this.canvas.current.height = this.height;
+
+      context.drawImage(this.video.current, 0, 0, this.width, this.height);
+      // const img = this.canvas.current.toDataURL('image/png');
+      // this.uploadImg(img);
+
+      this.setState(prevState => {
+        if (!prevState.isCanvasFilled) {
+          return { isCanvasFilled: !prevState.isCanvasFilled };
+        }
+      });
+    } else {
+      this.clearPhoto();
+    }
+  };
+
+  clearPhoto = () => {
+    const context = this.canvas.current.getContext('2d');
+    context.fillStyle = '#AAA';
+    context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  };
+
+  uploadImg = img => {
+    fetch(img)
+      .then(res => res.blob())
+      .then(blob => {
+        const formData = new FormData();
+        formData.append('file', blob);
+
+        // photosApi.upload(formData).then(data => console.warn('data', data));
+      });
+  };
+
+  downloadImg = () => {
+    this.canvas.current.toBlob(blob => {
+      let link = document.createElement('a');
+      link.download = `Snapshot_${new Date().toISOString()}.png`;
+
+      link.href = URL.createObjectURL(blob);
+      link.click();
+
+      URL.revokeObjectURL(link.href);
+    }, 'image/png');
+  };
+
+  render() {
+    const {
+      state: { isCanvasFilled },
+      width,
+      height,
+    } = this;
+
+    return (
+      <>
+        <h1>Webcam broadcast with snapshot func</h1>
+        <div className={styles.Container}>
+          <video ref={this.video} onCanPlay={this.handleCanPlay}>
+            Video stream not available.
+          </video>
+          <div className={styles.Home__canvas} style={{ width, height }}>
+            <canvas ref={this.canvas} />
+            {isCanvasFilled && (
+              <button onClick={this.downloadImg} className={styles.Home__button_dowload}>
+                Download
+              </button>
+            )}
+          </div>
+        </div>
+        <button
+          ref={this.startButton}
+          onClick={this.handleClick}
+          className={styles.Home__button_snapshot}
+        >
+          Take photo
+        </button>
+      </>
+    );
+  }
+}
+
+export default Home;
